6d908577d27bb5064acbbd9aef4092ef176b6065
Chris Boujoukos <github email address>
2018-09-25 17:26:49 -0400

    add Lists

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {lists: ""}
+    this.getLists = this.getLists.bind(this)
+  }
+  getLists(){
+    this.setState({lists: "These are my lists"})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h3> Movie Listr </h3>
         </header>
+        <button
+          onClick={this.getLists}
+        >
+        Get Lists
+        </button>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.lists}
         </p>
       </div>
     );
