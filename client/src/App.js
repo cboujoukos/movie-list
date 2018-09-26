0aed08c1383677ede36a0a53ec67c223e827cddc
Chris Boujoukos <github email address>
2018-09-26 09:32:29 -0400

    add login

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import axios from 'axios';
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {lists: ""}
+    this.state = {
+      lists: "",
+      email: "",
+      password: "",
+    }
     this.getLists = this.getLists.bind(this)
   }
   getLists(){
@@ -20,12 +24,73 @@ class App extends Component {
     })
   }
 
+  handleChange = (event) => {
+    console.log(event.target.name)
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+    let request = {"auth": {"email": this.state.email, "password": this.state.password}}
+    console.log(request)
+    axios.post('http://localhost:3001/api/user_token', {
+      data: request,
+      dataType: JSON,
+    })
+    .then(function(rsp){
+      console.log(rsp)
+    })
+    .catch(function(err){
+      console.log(err)
+    })
+  }
+
+  login(){
+    let email = this.state.email
+    let password = this.state.password
+    // axios.post('http://localhost:3001/api/user_token', {
+    //   email: this.state.email,
+    //   password: this.state.password
+    // })
+    console.log("Email: " + email + " Password: " + password)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h3> Movie Listr </h3>
         </header>
+
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <label htmlFor="email">Email: </label>
+          <br />
+          <input
+            name="email"
+            id="email"
+            type="email"
+            onChange={(event) => this.handleChange(event)}
+          />
+          <br /> <br />
+          <label htmlFor="password">Password: </label>
+          <br />
+          <input
+            name="password"
+            id="password"
+            type="password"
+            onChange={(event) => this.handleChange(event)}
+            />
+          <br />
+          <input
+            type="submit"
+            />
+        </form>
+        <br />
+        <button onClick={this.login}>Login</button>
+        <br />
+
         <button
           onClick={this.getLists}
         >
