e7e78a73fac7a2dcc6e9e65afb114eab0f1a2502
Chris Boujoukos <github email address>
2018-09-26 08:56:02 -0400

    add authenticate user to see lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
   scope '/api' do
     resources :lists
+    post 'user_token' => 'user_token#create'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
