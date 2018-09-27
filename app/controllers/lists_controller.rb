cbd9a795c73c3798110eedeb644055d6782ca06c
Chris Boujoukos <github email address>
2018-09-27 10:05:44 -0400

    limit lists#index to current_user's lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,7 @@ class ListsController < ApplicationController
 
   # GET /lists
   def index
-    @lists = List.all
+    @lists = List.where(user_id: current_user.id)
 
     render json: @lists
   end
