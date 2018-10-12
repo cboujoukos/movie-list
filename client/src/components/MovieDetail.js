8d3a9c3b3c3aa6fa44f7ab3fc13c2e82d1b534ef
Chris Boujoukos <cboujoukos@gmail.com>
2018-10-12 17:12:47 -0400

    style movie detail view

--- client/src/components/MovieDetail.js
+++ client/src/components/MovieDetail.js
@@ -5,8 +5,12 @@ class MovieDetail extends Component{
 
   render(){
     return(
-      <div>
+      <div className='movie-details'>
         <h1>{this.props.movie.movie.title}</h1>
+        <h3>Genre:</h3>
+        <p>{this.props.movie.movie.genre}</p>
+        <h3>Synopsis: </h3>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </div>
     )
   }
